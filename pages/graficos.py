46c7de37494859b5aa7922a8bb21272881a46fac
Yuvi <adm.yuvi@gmail.com>
2024-05-30 18:36:34 -0300

    Update graficos.py

--- pages/graficos.py
+++ pages/graficos.py
@@ -10,7 +10,7 @@ ad_acc = "956757482423549"
 fb_graph_api = GraphAPI(ad_acc, fb_api)
 
 st.header("Gráficos e Visualizações")
-campaign_status = pd.DataFrame(fb_graph_api.get_campaigns_status()['data'])
+campaign_status = pd.DataFrame(fb_graph_api.get_campaigns_status(ad_acc)['data'])
 campaign_status_count = campaign_status['status'].value_counts().reset_index()
 campaign_status_count.columns = ['status', 'count']
 campaign_id = "120208363974670077"
@@ -46,7 +46,7 @@ st.plotly_chart(fig)
 # Status dos Conjuntos de Anúncios
 st.header("Status dos Conjuntos de Anúncios")
 # Agrupar e contar o status dos conjuntos de anúncios
-adset_status = pd.DataFrame(fb_graph_api.get_adset_status()['data'])
+adset_status = pd.DataFrame(fb_graph_api.get_adset_status(ad_acc)['data'])
 adset_status_count = adset_status['status'].value_counts().reset_index()
 adset_status_count.columns = ['status', 'count']
 
