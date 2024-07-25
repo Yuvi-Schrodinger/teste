df70198e0c82b8278aaf1c4f98210f55380a9b1b
Yuvi <adm.yuvi@gmail.com>
2024-07-25 11:19:26 -0300

    Update graficos.py

--- pages/graficos.py
+++ pages/graficos.py
@@ -5,7 +5,7 @@ import streamlit as st
 from Isaloka import *
 
 
-fb_api = open("tokens/token").read()
+fb_api = st.secrets['db_token']
 ad_acc = "956757482423549"
 fb_graph_api = GraphAPI(ad_acc, fb_api)
 
